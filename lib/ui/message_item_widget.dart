295f341be2305ad44dfeb74c0c7c00576520b42a
Marvel <marvel@gmail.com>
2024-03-14 23:22:06 +0800

    Update message_item_widget.dart

--- lib/ui/message_item_widget.dart
+++ lib/ui/message_item_widget.dart
@@ -5,54 +5,70 @@ class MessageItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        // Image(image: NetworkImage("")),
-        SizedBox(width: 70, height: 40),
-        Expanded(
-          child: Container(
-            color: Colors.amber,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "李女士",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black45),
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                      "bibili-HRM",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Color(0xFF00888C)),
-                    ),
-                    Spacer(),
-                    Text(
-                      "15:09",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.normal,
-                          color: Color(0xFF00888C)),
-                    ),
-                    SizedBox(width: 8)
-                  ],
-                ),
-                Text("data")
-              ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(22),
+            child: Icon(
+              Icons.card_membership_outlined,
+              size: 44,
             ),
           ),
-        )
-      ],
+          SizedBox(width: 6),
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "李女士",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            color: Color(0XFF1E1E1E)),
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        "bibili-HRM",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            color: Color(0xFF909090)),
+                      ),
+                      Spacer(),
+                      Text(
+                        "15:09",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                            color: Color(0xFFB8B8B8)),
+                      ),
+                      // SizedBox(width: 8)
+                    ],
+                  ),
+                  SizedBox(height: 3),
+                  Text.rich(
+                    TextSpan(text: "[已读] ", children: [
+                      TextSpan(
+                          text: "嗯，好的",
+                          style: TextStyle(color: Color(0xFF595959)))
+                    ]),
+                    style: TextStyle(color: Color(0xFFB8B8B8), fontSize: 14),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
