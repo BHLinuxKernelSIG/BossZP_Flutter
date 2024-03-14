6467f222c7a0fb741b29c1846a3ffada6cd77547
Marvel <marvel@gmail.com>
2024-03-14 23:04:45 +0800

    1

--- lib/ui/message_item_widget.dart
+++ lib/ui/message_item_widget.dart
@@ -7,14 +7,50 @@ class MessageItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Image(image: NetworkImage("")),
-        Column(
-          children: [
-            Row(
-              children: [Text("data"), Text("data")],
+        // Image(image: NetworkImage("")),
+        SizedBox(width: 70, height: 40),
+        Expanded(
+          child: Container(
+            color: Colors.amber,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "李女士",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black45),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "bibili-HRM",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF00888C)),
+                    ),
+                    Spacer(),
+                    Text(
+                      "15:09",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF00888C)),
+                    ),
+                    SizedBox(width: 8)
+                  ],
+                ),
+                Text("data")
+              ],
             ),
-            Text("data")
-          ],
+          ),
         )
       ],
     );
