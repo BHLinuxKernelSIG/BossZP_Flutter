16b0e2ed26f7b77e4c6a070a225e20388ba52bb7
Marvel <marvel@gmail.com>
2024-03-14 21:49:44 +0800

    Update position_item_widget.dart

--- lib/ui/position_item_widget.dart
+++ lib/ui/position_item_widget.dart
@@ -13,11 +13,11 @@ class PostionItemWidget extends StatelessWidget {
     return Container(
       color: Color(0xFFF2F2F4),
       child: Card(
-        margin: EdgeInsets.only(left: 10, right: 10, top: 14, bottom: 0),
+        margin: EdgeInsets.only(left: 10, right: 10, top: 12, bottom: 0),
         elevation: 0.2,
         color: Colors.white,
         child: Padding(
-          padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
+          padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -28,7 +28,7 @@ class PostionItemWidget extends StatelessWidget {
                       "iOS开发工程师",
                       overflow: TextOverflow.ellipsis,
                       style: TextStyle(
-                          fontSize: 18,
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
                           color: Colors.black87),
                     ),
@@ -38,7 +38,7 @@ class PostionItemWidget extends StatelessWidget {
                     "20-25K",
                     textAlign: TextAlign.end,
                     style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 16,
                         fontWeight: FontWeight.normal,
                         color: Color(0xFF00888C)),
                   )
@@ -47,7 +47,7 @@ class PostionItemWidget extends StatelessWidget {
               SizedBox(height: 8),
               Text(
                 "腾讯 不需要融资 1000人以上",
-                style: TextStyle(fontSize: 14, color: colorDark),
+                style: TextStyle(fontSize: 12, color: colorDark),
               ),
               SizedBox(height: 8),
               Wrap(
@@ -63,13 +63,13 @@ class PostionItemWidget extends StatelessWidget {
                       margin: EdgeInsets.fromLTRB(0, 0, 4, 0),
                       padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
                       child: Text(
-                        "iii000i$i",
-                        style: TextStyle(color: colorDark, fontSize: 12),
+                        "经验不限$i",
+                        style: TextStyle(color: colorDark, fontSize: 10),
                       ),
                     )
                 ],
               ),
-              SizedBox(height: 12),
+              SizedBox(height: 8),
               Row(
                 children: [
                   Icon(Icons.ac_unit_rounded),
@@ -79,25 +79,25 @@ class PostionItemWidget extends StatelessWidget {
                     children: [
                       Text(
                         "张先生 - 人事总监",
-                        style: TextStyle(fontSize: 12, color: Colors.black87),
+                        style: TextStyle(fontSize: 10, color: Colors.black87),
                       ),
                       if (Random().nextBool())
                         Text(
                           "今日回复4次",
-                          style: TextStyle(fontSize: 12, color: colorDark),
+                          style: TextStyle(fontSize: 10, color: colorDark),
                         )
                     ],
                   ),
                   Spacer(),
                   Text(
                     "东城区 - 建国门",
-                    style: TextStyle(fontSize: 12, color: colorDark),
+                    style: TextStyle(fontSize: 10, color: colorDark),
                   ),
                   IconButton(
                     splashColor: Colors.transparent,
                     highlightColor: Colors.transparent,
                     padding: EdgeInsets.zero,
-                    iconSize: 18,
+                    iconSize: 16,
                     color: colorDark,
                     onPressed: () {},
                     icon: Icon(Icons.close),
