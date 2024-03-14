8ea5bdcc0e221edfc98e2492bb8f977b6f86d5d7
Marvel <marvel@gmail.com>
2024-03-14 18:04:13 +0800

    Update posotion_list_widget.dart

--- lib/ui/posotion_list_widget.dart
+++ lib/ui/posotion_list_widget.dart
@@ -14,26 +14,29 @@ class PositionListWidget extends StatelessWidget {
               margin: EdgeInsets.only(left: 6, right: 6, top: 0, bottom: 6),
               elevation: 0.2,
               color: Colors.white24,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "iOS开发工程师",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        "20-25K",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.normal),
-                      )
-                    ],
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "iOS开发工程师",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "20-25K",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           );
