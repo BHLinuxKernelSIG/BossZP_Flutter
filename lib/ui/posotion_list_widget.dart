4f8b91f286d9c95315fc50742613355f105f741c
Marvel <marvel@gmail.com>
2024-03-14 20:47:07 +0800

    1

--- lib/ui/posotion_list_widget.dart
+++ lib/ui/posotion_list_widget.dart
@@ -9,45 +9,49 @@ class PositionListWidget extends StatelessWidget {
         itemCount: 100,
         itemBuilder: (context, index) {
           return SizedBox(
-            height: 100,
-            child: Card(
-              margin: EdgeInsets.only(left: 6, right: 6, top: 0, bottom: 6),
-              elevation: 0.2,
-              color: Colors.white24,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            "iOS开发工程师",
-                            overflow: TextOverflow.ellipsis,
+            // height: 100,
+            child: Container(
+              color: Color(0xFFF2F2F4),
+              child: Card(
+                margin: EdgeInsets.only(left: 6, right: 6, top: 0, bottom: 6),
+                elevation: 0.2,
+                color: Colors.white,
+                // surfaceTintColor: Colors.green,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "iOS开发工程师",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "20-25K",
+                            textAlign: TextAlign.end,
                             style: TextStyle(
                                 fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87),
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "20-25K",
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.greenAccent),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "腾讯 不需要融资 1000人以上",
-                      style: TextStyle(fontSize: 14, color: Colors.black54),
-                    ),
-                  ],
+                                fontWeight: FontWeight.normal,
+                                color: Colors.greenAccent),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "腾讯 不需要融资 1000人以上",
+                        style: TextStyle(fontSize: 14, color: Colors.black54),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
